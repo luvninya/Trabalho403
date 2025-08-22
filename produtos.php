de3f3cc72cc526768db677b52fff1a560be98a96
Otavio_Rohr <60030053005@senacrs.edu.br>
2025-08-22 09:22:48 -0300

    v0.2

--- produtos.php
+++ produtos.php
@@ -1,58 +1,16 @@
 <?php
-// ---------------------------
-// Constantes de conexão
-// ---------------------------
+
 define('DBSERVER', 'localhost');  // servidor do banco de dados
 define('DBUSER', 'root');         // usuário do MySQL
 define('DBPASS', '');             // senha de acesso ao MySQL
 define('DBBASE', 'empresa');      // nome da base de dados
 
-// ---------------------------
-// Conexão com o banco
-// ---------------------------
 $conexao = mysqli_connect(DBSERVER, DBUSER, DBPASS, DBBASE);
 
-// Verifica se a conexão deu certo
 if (!$conexao) {
     die("Falha na conexão: " . mysqli_connect_error());
 }
 
-// ---------------------------
-// Consulta à tabela CARGOS
-// ---------------------------
-$sql = "SELECT * FROM cargos";
-$resultado = mysqli_query($conexao, $sql);
-
-if (mysqli_num_rows($resultado) > 0) {
-    echo "<h2>Cargos</h2>";
-    while ($row = mysqli_fetch_assoc($resultado)) {
-        echo "Cargo: " . $row["Nome"] . " | Teto Salarial: R$ " . $row["TetoSalarial"] . "<br>";
-    }
-} else {
-    echo "Nenhum cargo encontrado.<br>";
-}
-
-// ---------------------------
-// Consulta à tabela SETOR
-// ---------------------------
-$sqlSetor = "SELECT * FROM setor";
-$resultadoSetor = mysqli_query($conexao, $sqlSetor);
-
-if (mysqli_num_rows($resultadoSetor) > 0) {
-    echo "<h2>Selecione um Setor</h2>";
-    echo '<select name="setor" id="setor">';
-    while ($row = mysqli_fetch_assoc($resultadoSetor)) {
-        // Substitua 'id' e 'Nome' pelos nomes exatos das colunas da sua tabela
-        echo '<option value="' . $row['id'] . '">' . $row['Nome'] . '</option>';
-    }
-    echo '</select>';
-} else {
-    echo "Nenhum setor encontrado.";
-}
-
-// ---------------------------
-// Consulta à tabela Produtos
-// ---------------------------
 $sql = "SELECT * FROM produtos";
 $resultado = mysqli_query($conexao, $sql);
 
@@ -76,40 +34,8 @@ if (mysqli_num_rows($resultadoProdutos) > 0) {
     }
     echo '</select>';
 } else {
-    echo "Nenhum setor encontrado.";
+    echo "Nenhum produto encontrado.";
 }
 
-// ---------------------------
-// Consulta à tabela Funcionarios
-// ---------------------------
-$sql = "SELECT * FROM funcionarios";
-$resultado = mysqli_query($conexao, $sql);
-
-if (mysqli_num_rows($resultado) > 0) {
-    echo "<h2>Funcionarios</h2>";
-    while ($row = mysqli_fetch_assoc($resultado)) {
-        echo "Funcionarios: " . $row["Nome"] . "<br>";
-    }
-} else {
-    echo "Nenhum cargo encontrado.<br>";
-}
-
-$sqlFuncionarios = "SELECT * FROM funcionarios";      
-$resultadoFuncionarios = mysqli_query($conexao, $sqlFuncionarios);
-
-if (mysqli_num_rows($resultadoFuncionarios) > 0) {
-    echo "<h2>Selecione um Funcionario</h2>";
-    echo '<select name="funcionarios" id="funcionarios">';
-    while ($row = mysqli_fetch_assoc($resultadoFuncionarios)) {
-        echo '<option value="' . $row['id'] . '">' . $row['Nome'] . '</option>';
-    }
-    echo '</select>';
-} else {
-    echo "Nenhum funcionario encontrado.";
-}
-
-
-// ---------------------------
-// Fecha a conexão
 mysqli_close($conexao);
 ?>
